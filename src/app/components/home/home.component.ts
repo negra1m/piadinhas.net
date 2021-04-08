eea0d53aecaec96c41e992f72e5756773e38dd6d
negra1m <vnsn_@outlook.com>
2021-04-07 21:20:40 -0300

    feat(home): finalizando componente home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  active: boolean;
+  buttonText = "Gerar"
+  piadinha: any;
+  url = 'https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas'
+
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    this.active = false;
+  }
+
+  gerarPiada(){
+    this.active = true;
+    this.buttonText = "Nova";
+    this.getPiada();
+  }
+
+  getPiada() {
+    const headers = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    this.http.get(this.url, headers)
+    .subscribe(res => {
+      this.piadinha = res;
+    });
   }
 
 }
